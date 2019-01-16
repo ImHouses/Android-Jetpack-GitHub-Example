5e36e96f615c0b335b196d5c9d27abcc3ea23d5e
Juan Casas <Imhouses@ciencias.unam.mx>
2019-01-16 13:03:23 -0600

    Changed: Styles for the text of the views.

--- app/src/main/java/com/example/jcasas/githubjetpackexample/presentation/repositories/adapter/RepositoriesAdapter.kt
+++ app/src/main/java/com/example/jcasas/githubjetpackexample/presentation/repositories/adapter/RepositoriesAdapter.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.jcasas.githubjetpackexample.R
 import com.example.jcasas.githubjetpackexample.data.model.GitHubRepository
 import kotlinx.android.synthetic.main.repository_item.view.*
@@ -17,6 +19,12 @@ class RepositoriesAdapter(
         fun bindData(repo: GitHubRepository) {
             bindTitle(repo.name)
             view.textDescriptionRepoItem.text = repo.description
+            Glide
+                .with(view.context)
+                .load(repo.owner.avatarUrl)
+                .apply(RequestOptions.circleCropTransform())
+                .into(view.imageAvatarRepoItem)
+            view.textOwnerNameRepoItem.text = repo.owner.username
         }
 
         private fun bindTitle(name: String) {
